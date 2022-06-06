44defdc8aa81ab19799b5f641c989f93b368051b
albaradolucas <lucasalb0120@gmail.com>
2022-06-06 11:21:25 -0300

    Exercise 2 done

---
+++ Exercise_2/index.js
@@ -0,0 +1,12 @@
+const lista = [
+    nombre = "Lucas",
+    edad = 20,
+    desarrollador = true,
+    cumpleaños = newDate(2001, 15, 8),
+    libroFavorite = {
+        titulo : "El psicoanalista",
+        autor : "John Katzenbach",
+        fecha : newDate(2002),
+        url : "https://resumiendolo.com/c-novela-negra/el-psicoanalista/"
+    }
+]
