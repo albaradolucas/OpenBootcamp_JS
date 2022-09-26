3666fb289ebaf0aa8e0c41b25aee8c72a5e598cf
albaradolucas <lucasalb0120@gmail.com>
2022-09-26 09:35:18 -0300

    relocate and continue

---
+++ exe 3/factorial-break.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 20; i++) {
+    console.log(i);
+    
+    if(i<=10) {
+        console.log(i);
+        continue;
+    }
+    else {
+        break;
+    }
+}
---
+++ exe 3/factorial-while.js
@@ -0,0 +1,6 @@
+let i = 0;
+let max = 10;
+
+while (i <= max) {
+    console.log(i);
+}
