3f9d56411101b3712d92311754dea4010824b418
albaradolucas <lucasalb0120@gmail.com>
2022-09-26 09:53:52 -0300

    Create index.js

---
+++ exe 4/index.js
@@ -0,0 +1,19 @@
+const nombre = "Lucas ";
+const apellido = "González";
+const estudiante = nombre + apellido;
+const estudianteMayus = estudiante;
+const estudianteMinus = estudiante;
+const numletras = estudiante;
+const primlet = nombre;
+const ultlet = apellido;
+const nospace = estudiante;
+const esta = nombre;
+
+console.log(estudiante);
+console.log(estudianteMayus.toUpperCase());
+console.log(estudianteMinus.toLowerCase());
+console.log(numletras.length);
+console.log(primlet.charAt());
+console.log(ultlet.charAt(7));
+console.log(nospace.trim());
+console.log(estudiante.includes(nombre))
