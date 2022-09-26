40cdb85f1dcf043094410a6da3060ee71c61a537
albaradolucas <lucasalb0120@gmail.com>
2022-09-26 10:10:23 -0300

    Create numeros.js

---
+++ exe 5/numeros.js
@@ -0,0 +1,11 @@
+const altcm = 172;
+const altm = 1.72;
+const pesokg = 63.7;
+const altmred = altm.toFixed();
+const pesokgabajo = Math.floor(pesokg);
+
+console.log(altcm);
+console.log(altm);
+console.log(pesokg);
+console.log(altmred);
+console.log(pesokgabajo);
